5260d5097cc19d7770c3055d24a2c0825d1c3e31
Pierre Merlin <merlinp82@gmail.com>
2014-02-17 22:37:51 +0100

    Add log_test.php

---
+++ tests/log_test.php
@@ -0,0 +1,8 @@
+<?php
+// from http://www.simpletest.org/fr/start-testing.html
+require_once('simpletest/autorun.php');
+require_once('../classes/log.php');
+
+class TestOfLogging extends UnitTestCase {
+}
+?>
