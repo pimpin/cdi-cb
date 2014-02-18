542da7734483da1cf08905010288040d79c5b343
nitrousGG <nitrousboxGG@gmail.com>
2014-02-18 10:26:43 +0000

    Fix test runner

--- tests/run.php
+++ tests/run.php
@@ -4,7 +4,7 @@ require_once('simpletest/autorun.php');
 class MyTest extends TestSuite {
     function __construct() {
         parent::__construct();
-        $this->addFile('log_test.php');
+      $this->addFile(dirname(__FILE__).'/log_test.php');
     }
 }
 ?>
