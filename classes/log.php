2f857a791bfe51e1aef9dc6f34dfb8a67672e6b0
Pierre Merlin <pierrem@apido.org>
2014-02-18 16:12:23 +0100

    feature create file done close #17

--- classes/log.php
+++ classes/log.php
@@ -1,6 +1,12 @@
 <?php
 class Log {
-  function Log($log_path){
+  protected $log_path;
+  function __construct($log_path)
+  {
+    $this->log_path = $log_path;
+  }
+  function message($message)
+  {
+    fopen($this->log_path, 'w+');
   }
 }
-?>
--- tests/log_test.php
+++ tests/log_test.php
@@ -6,8 +6,14 @@ require_once(dirname(__FILE__).'/../classes/log.php');
 class TestOfLogging extends UnitTestCase {
   function testLogCreateNewFileOnFirstMessage() {
     @unlink('/temp/test.log');
-    $log = new Log('/temp/test.log');
+    $log = new Log(dirname(__FILE__).'/../temp/test.log');
     $this->assertFalse(file_exists('/temp/test.log'));
+    $log->message('Should write this to a new file');
+    $this->assertTrue(file_exists(dirname(__FILE__).'/../temp/test.log'));
+    @unlink('/temp/test2.log');
+    $log2 = new Log(dirname(__FILE__).'/../temp/test2.log');
+    $this->assertFalse(file_exists('/temp/test2.log'));
+    $log2->message('Should write this to an other file');
+    $this->assertTrue(file_exists(dirname(__FILE__).'/../temp/test2.log'));
   }
 }
-?>
